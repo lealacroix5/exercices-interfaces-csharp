708940770680aac3d41e837277b7c03bf7186c3b
Mililas <poulpimarvel@gmail.com>
2026-09-21 14:37:05 +0200

    Exercice03: Variable de type interface

---
+++ Exercice03/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello, World!");
+
+IAffichable element = new Produit("Clavier", 49.90m);
+element.Afficher();
+
+element = new Client("Alice", "alice@example.com");
+element.Afficher();
