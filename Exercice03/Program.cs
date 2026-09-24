950ce67dcea02390d22cdff9366ace380095c35a
Mililas <poulpimarvel@gmail.com>
2026-09-24 09:20:56 +0200

    reponse aux questions

--- Exercice03/Program.cs
+++ Exercice03/Program.cs
@@ -5,3 +5,19 @@ element.Afficher();
 
 element = new Client("Alice", "alice@example.com");
 element.Afficher();
+
+// RÉPONSES AUX QUESTIONS
+
+// 1. Le type de la variable element est IAffichable.
+
+// 2. Dans le premier exemple, le type réel
+// de l'objet est Produit.
+
+// 3. Dans le deuxième exemple, le type réel
+// de l'objet est Client.
+
+// 4. element.Afficher() fonctionne dans les deux cas
+// car Produit et Client implémentent IAffichable.
+
+// 5. Non, on ne peut pas écrire element.Prix,
+// car Prix n'est pas défini dans IAffichable.
